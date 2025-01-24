b7d138ed6d72633f125b7c27ccf144517239ec0f
Manishpande2 <biraalogang@gmail.com>
2025-01-24 21:09:06 +0545

    react useState function learned

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const About = () => {
+  const [set, setCount] = useState(0);
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
   return (
     <>
-      <h1>Hello I am from about</h1>
+      <h1>{set}</h1>
+      <button onClick={() => setCount(set + 1)}>+</button>
+      <button onClick={decrement}>-</button>
       <h2>Do you see me</h2>
       <Button name="About us" />
     </>
